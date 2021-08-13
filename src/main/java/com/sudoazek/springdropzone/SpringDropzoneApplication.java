ea9d79145f4c6143dbacab17d717ab6a63950dc2
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2021-08-13 12:04:30 +0900

    initial commit

---
+++ src/main/java/com/sudoazek/springdropzone/SpringDropzoneApplication.java
@@ -0,0 +1,13 @@
+package com.sudoazek.springdropzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDropzoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDropzoneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sudoazek/springdropzone/SpringDropzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudoazek.springdropzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDropzoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
